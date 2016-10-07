9db6bc37ed0c5a800c4258eff7f5dea0c4171f18
Edward Ritter <esritter@gmail.com>
2016-10-07 13:58:15 -0400

    Move templates into separate view files.

--- src/swank-bootstrap.js
+++ src/swank-bootstrap.js
@@ -2,6 +2,12 @@
 (function(angular) {
   'use strict';
 
+  /**
+   * LoDashFactory - makes lodash injectable.
+   * Throws an error if it can't be found
+   * @param {object} $window angular window object
+   * @param {object} $log    angular log object
+   */
   function LoDashFactory($window, $log) {
     try {
       if (!$window._) {
@@ -14,6 +20,10 @@
   }
   LoDashFactory.$inject = ['$window', '$log'];
 
+  /**
+   * SwankParseMDFilter - filter for parsing markdown to HTML
+   * @param {object} $log angular log object
+   */
   function SwankParseMDFilter($log) {
     return function(content) {
       try {
@@ -31,6 +41,9 @@
   }
   SwankParseMDFilter.$inject = ['$log'];
 
+  /**
+   * SwankSelectify - formats input to produce a unique ID
+   */
   function SwankSelectify() {
     return function(slug) {
       slug = slug.replace('{', '/');
@@ -45,6 +58,10 @@
   }
   SwankSelectify.$inject = [];
 
+  /**
+   * SwankBootstrapController - root controller
+   * @param {object} Swank swank object from swank core
+   */
   function SwankBootstrapController(Swank) {
     angular.extend(this, {
       api: new Swank(this.api)
@@ -60,138 +77,39 @@
   var SwankBootstrapComponent = {
     bindings: {api: '<'},
     controller: SwankBootstrapController,
-    template: [
-      '<div class="container">',
-        '<div class="row">',
-          '<div class="col-12">',
-            '<swank-info></swank-info>',
-          '</div>',
-          '<div class="col-12">',
-            '<swank-paths></swank-paths>',
-          '</div>',
-          '<div class="col-12">',
-            '<div class="row">',
-              '<swank-tags></swank-tags>',
-            '</div>',
-        '</div>',
-      '</div>',
-    '</div>'
-    ].join('\n')
+    templateUrl: 'views/swank-bootstrap.html'
   };
 
   var SwankInfoBlockComponent = {
     require: {swank: '^swankBootstrap'},
-    template: [
-      '<div class="panel panel-default">',
-        '<div class="panel-heading">',
-          '<h3 class="panel-title">{{$ctrl.swank.api.doc.info.title}} <small>Version {{$ctrl.swank.api.doc.info.version}}</h3>',
-        '</div>',
-        '<div class="panel-body">',
-          '<div ng-if="$ctrl.swank.api.doc.info.description">',
-            '<h3>Description</h3>',
-            '<div ng-bind-html="$ctrl.swank.api.doc.info.description | parseMD"></div>',
-          '</div>',
-          '<div ng-if="$ctrl.swank.api.doc.info.termsOfService">',
-            '<h3>Description</h3>',
-            '<div ng-bind-html="$ctrl.swank.api.doc.info.termsOfService | parseMD"></div>',
-          '</div>',
-        '</div>',
-      '</div>'
-    ].join('\n')
+    templateUrl: 'views/swank-info-block.html'
   };
 
   var SwankPathsComponent = {
     require: {swank: '^swankBootstrap'},
-    template: [
-      '<div class="panel-group" id="pathslist" role="tablist" aria-multiselectable="true">',
-        '<div class="panel panel-primary" ng-repeat="(route, path) in $ctrl.swank.api.doc.paths">',
-          '<swank-path route="route" path="path"></swank-path>',
-        '</div>',
-      '</div>'
-    ].join('\n')
+    templateUrl: 'views/swank-paths.html'
   };
 
   var SwankPathComponent = {
     require: {paths: '^swankPaths'},
     bindings: {route: '<', path: '<'},
-    template: [
-      '<div class="panel-heading" role="tab" id="{{$ctrl.route | selectify}}">',
-        '<h4 class="panel-title">',
-          '<a role="button" data-toggle="collapse" data-parent="#pathslist" href="#collapse{{$ctrl.route | selectify}}" aria-expanded="false" aria-controls="collapse{{$ctrl.route | selectify}}">',
-            '{{$ctrl.route}}',
-          '</a>',
-        '</h4>',
-      '</div>',
-      '<div id="collapse{{$ctrl.route | selectify}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="{{$ctrl.route | selectify}}">',
-        '<div class="panel-body">',
-          '<swank-operation></swank-operation>',
-          // '<pre>{{$ctrl.path | json}}</pre>',
-        '</div>',
-      '</div>',
-    ].join('\n')
+    templateUrl: 'views/swank-path.html'
   };
 
   var SwankOperationComponent = {
     require: {path: '^swankPath'},
-    template: [
-      '<div class="panel panel-default" ng-repeat="(method,operation) in $ctrl.path.path track by $index">',
-        '<div class="panel-heading"><h3 class="panel-title">',
-          '<span class="label label-default">{{method}}</span> {{$ctrl.path.route}} <span class="pull-right">{{operation.summary}}</span>',
-        '</h3></div>',
-        '<div class="panel-body">',
-          '<h4>Implementation Notes</h4>',
-          '<div ng-bind-html="operation.description | parseMD"></div>',
-          '<h4 ng-if="operation.parameters.length > 0">Parameters</h4>',
-          '<swank-parameters ng-if="operation.parameters.length > 0" parameters="operation.parameters">',
-          '</swank-parameters>',
-          '<h4>Response Messages</h4>',
-          '<swank-responses responses="operation.responses">',
-          '</swank-responses>',
-        '</div>',
-      '</div>'
-    ].join('\n')
+    templateUrl: 'views/swank-operation.html'
   };
 
   var SwankParametersComponent = {
     bindings: {parameters: '<'},
     transclude: true,
-    template: [
-    '<table class="table">',
-      '<tr>',
-        '<th>Parameter</th>',
-        '<th>Description</th>',
-        '<th>Parameter Type</th>',
-        '<th>Data Type</th>',
-      '</tr>',
-      '<tr ng-repeat="parameter in $ctrl.parameters" parameter="parameter">',
-        '<td>{{parameter.name}}</td>',
-        '<td>{{parameter.description}}</td>',
-        '<td>{{parameter.in}}</td>',
-        '<td>{{parameter.type}}</td>',
-      '</tr>',
-    '</table>'
-    // '<div><swank-parameter ng-repeat="parameter in $ctrl.parameters" parameter="parameter"></swank-parameter></div>'
-    ].join('\n')
+    templateUrl: 'views/swank-parameters.html'
   };
 
   var SwankResponsesComponent = {
     bindings: {responses: '<'},
-    template: [
-      '<table class="table">',
-      '<tr>',
-        '<th>HTTP Status Code</th>',
-        '<th>Reason</th>',
-        '<th>Response Model</th>',
-        '<th>Headers</th>',
-      '</tr>',
-      '<tr ng-repeat="(number,response) in $ctrl.responses">',
-        '<td>{{number}}</td>',
-        '<td>{{response.description}}</td>',
-        '<td>{{response.schema}}</td>',
-        '<td>{{response.headers}}</td>',
-      '</tr>',
-    '</table>'
-    ].join('\n')
+    templateUrl: 'views/swank-responses.html'
   };
 
   var SwankTagsComponent = {
